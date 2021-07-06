b13f34e9c18049738956b1ee465bc45b02b419a3
cristianocsl <cslcristiano@gmail.com>
2021-07-06 12:24:49 -0300

    Adicione, no diretório src , um componente chamado StrictAccess. Se username e password de user for "joao" e "1234", renderiza o StrictAccess

--- src/App.js
+++ src/App.js
@@ -14,20 +14,20 @@ class App extends Component {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/users">Users</Link></li>
-            <li><Link to="/strictaccess" >StrictAccess</Link></li>
+            <li><Link to="/strict-access" >Strict Access</Link></li>
           </ul>
         </nav>
         <Switch>
           <Route
-            exact path="/users/:id"
+            path="/users/:id"
             render={(props) => <Users {...props} greetingsMessage="Good Morning" />}
           />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/" component={Home} />
           <Route
-            exact path="/strictacess"
-            render={(props) => <StrictAccess {...props} user={ [username] = 'joao', [password] = '1234' } />}
+            path="/strict-access"
+            render={() => <StrictAccess user={ {username: 'joao', password: '1234'} } />}
           />
+          <Route path="/about" component={About} />
+          <Route exact path="/" component={Home} />
         </Switch>
       </BrowserRouter>
     );
